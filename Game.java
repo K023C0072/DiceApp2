acc6101ffa89d85b537da20d0b1391274c994179
K023C0072 <k023c0072@g.neec.ac.jp>
2024-12-12 14:20:39 +0900

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,22 @@
+public class Game {
+  Player p;
+  Dice d;
+
+
+  Game (){
+
+  p = new Player();
+  d = new Dice();
+
+  }
+
+
+  void start(){
+    p.turn(d);
+    System.out.println(d.me);
+  }
+
+  void judge() {
+
+  }
+}
